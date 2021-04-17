4c3b2c3003a87630996a62160e13801a3d0cbd3e
sw1227 <32032108+sw1227@users.noreply.github.com>
2021-04-17 20:43:45 +0900

    add circle layer of stations

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -5,19 +5,43 @@
 <script lang="ts">
 import Vue from 'vue'
 import mapboxgl, { MapboxOptions } from 'mapbox-gl'
+import { FeatureCollection } from 'geojson'
+import gsix60 from '../data/gsix60.json'
 
 const options: MapboxOptions = {
   accessToken: process.env.VUE_APP_MAPBOX_TOKEN,
   container: 'mapbox',
   style: 'mapbox://styles/mapbox/light-v10',
-  center: [138, 38],
-  zoom: 8
+  center: [139.7, 35.7],
+  zoom: 9
 }
 
 export default Vue.extend({
   name: 'reachability',
+  data () {
+    return {
+      map: undefined as (undefined | mapboxgl.Map)
+    }
+  },
   mounted () {
     const map = new mapboxgl.Map(options)
+    map.on('load', () => {
+      map.addSource('stations', {
+        type: 'geojson',
+        data: gsix60 as FeatureCollection
+      })
+      const stationLayer: mapboxgl.CircleLayer = {
+        id: 'stations',
+        type: 'circle',
+        source: 'stations',
+        paint: {
+          'circle-radius': 6,
+          'circle-color': '#0ff'
+        }
+      }
+      map.addLayer(stationLayer)
+    })
+    this.map = map
   }
 })
 </script>
